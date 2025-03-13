2f4c122240c1dcd7cdfccc955583c1563cc20909
Catalin Voicu <catavoicu01@gmail.com>
2025-03-13 16:12:10 +0200

    Al treilea commit

---
+++ Layout/Indesign/StubI.vhd
@@ -0,0 +1,4 @@
+-- Generated PORTMAP Stub File: Created by Capture FPGA Flow
+-- Matches PCB component pinout with simulation model
+-- Created Saturday, May 25, 2024 19:57:16 GTB Daylight Time
+
---
+++ Layout/Indesign/sdf.cmd
@@ -0,0 +1,2 @@
+LOG_FILE = "c:\users\catav\onedrive - universitatea politehnica bucuresti\desktop\tie\Indesign\sdfout.log",
+SCOPE = :dut;
